ccd4212d0a4e68a2592f51e62510121feb27357c
varun053101 <varun05310104@gmail.com>
2026-03-05 00:55:39 +0530

    standards: error response was not in the standard form

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -1,16 +1,25 @@
 const rateLimit = require("express-rate-limit");
 require("dotenv").config();
+const { successResponse, errorResponse } = require("../utils/response");
 
 const loginLimiter = rateLimit({
   windowMs: 5 * 60 * 1000, // 5 minutes
-  max: parseInt(process.env.LOGINLIMITER_MAX), // limit each IP to required number of requests per window
-  message: { error: "Too many login attempts, please try again later." },
+  max: parseInt(process.env.LOGINLIMITER_MAX) || 5, // limit each IP to required number of requests per window
+  handler: (req, res) => {
+    errorResponse(res, 429, "Too many login attempts, please try again later.");
+  },
 });
 
 const registerLimiter = rateLimit({
   windowMs: 10 * 60 * 1000, // 10 minutes
-  max: parseInt(process.env.REGISTERLIMITER_MAX), // limit each IP to required number of requests per window
-  message: { error: "Too many registration attempts, please try again later." },
+  max: parseInt(process.env.REGISTERLIMITER_MAX) || 3, // limit each IP to required number of requests per window
+  handler: (req, res) => {
+    errorResponse(
+      res,
+      429,
+      "Too many registration attempts, please try again later.",
+    );
+  },
 });
 
 module.exports = { loginLimiter, registerLimiter };
